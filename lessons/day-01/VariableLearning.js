e49cf889d52b662d4caa71900305b562c3359876
TranAnIT <an.tran.itpnv@gmail.com>
2024-06-20 17:39:46 +0700

    Update Variable

--- lessons/day-01/VariableLearning.js
+++ lessons/day-01/VariableLearning.js
@@ -1,2 +1,13 @@
 let myName = "Teo";
+console.log(myName);
+
+//[v] re-assign | [X] Re-declare
 myName = 19;
+console.log(myName);
+
+//Constant
+const PI_NUMBER = 3.14;
+//[x] Not allowed to be reassigned, ex: PI_NUMBER = 2
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
